565957adfaa2c01bee1c4383492b123d453e13bb
Maksim Tusaev <tusaev.maxim@yandex.ru>
2023-04-06 22:34:39 +0400

    add agregate count

--- Task3.sql
+++ Task3.sql
@@ -1,7 +1,11 @@
 SELECT
-	o.product_name
+	o.product_name,
+	count(o.amount) as amount_cnt
 FROM 
 	orders AS o 
 	LEFT JOIN customers AS c ON c.id = o.customer_id 
 WHERE
 	upper(c.name) = upper('alexey')
+GROUP BY
+	o.product_name
+	
